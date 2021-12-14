51e158a5233f4346fc10092fddf756208c6c6474
Mikhail <domiur@yandex.ru>
2021-12-14 17:25:15 +0300

    add hover

--- webcam/plotmotion.py
+++ webcam/plotmotion.py
@@ -1,10 +1,21 @@
+from bokeh.models.annotations import Tooltip
 from video import df 
 from bokeh.plotting import output_file,show,figure
+from bokeh.models import HoverTool,ColumnDataSource
+
+
+df['start_str']=df['start'].dt.strftime("%Y-%m-%d %H:%M:%S")
+df['end_str']=df['end'].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds=ColumnDataSource(df)
 
 f=figure(width=500,height=500,x_axis_type="datetime")
 f.yaxis.minor_tick_line_color=None
-#f.ygrid.ticker.desired_num_ticks=1
-f.quad(left=df["start"],right=df["end"],top=1,bottom=0,color='green')
+
+hover=HoverTool(tooltips=[("Start","@start_str"),("End","@end_str")])
+f.add_tools(hover)
+
+f.quad(left="start",right="end",top=1,bottom=0,color='green',source=cds)
+
 output_file("2.html")
 show(f)
 
