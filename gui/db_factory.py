03f4a409f8d0c396004488fb6e28c957381a8192
Mikhail <domiur@yandex.ru>
2021-12-29 10:18:57 +0300

    polish

--- gui/db_factory.py
+++ gui/db_factory.py
@@ -6,6 +6,7 @@ def db_connect(dbtype="sqlite3"):
         return db_sqlite3.db_connect
     elif dbtype=="postgres":
         return db_postgres.db_connect
+    print('unknown DB type')
     return None
 
     
